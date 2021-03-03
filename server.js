516a3bf9bed5bfb096d09e2c1fbfd76a11fe317b
agustinxmtz <70080354+agustinxmtz@users.noreply.github.com>
2021-03-03 10:59:35 -0600

    Getting the server to read the data properly by using app.use

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const { query } = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
   // Note that we save that animalsArray as filteredResults here:
